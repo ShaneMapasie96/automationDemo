1e7e120c75d8348032658df46688e7b7e2996ed4
Shane Jaftha <60961405+ShaneMapasie96@users.noreply.github.com>
2026-09-08 09:12:41 +0200

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,27 +1,37 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserTest {
 
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://shanemapasie-h5b4b2gcfxbzf5gu.southafricanorth-01.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		driver.close();
-	}
+    @Test
+    public void getData() {
+
+        System.out.println("Hello Guys");
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://shanemapasie-h5b4b2gcfxbzf5gu.southafricanorth-01.azurewebsites.net/webapp/");
+
+            String text = driver.findElement(By.cssSelector("h1")).getText();
+
+            System.out.println(text);
+
+            Assert.assertEquals(
+                "RahulShettyAcademy.com Learning",
+                text
+            );
+
+        } finally {
+            driver.quit();
+        }
+    }
 }
